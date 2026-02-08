9331d0580836f95392540b38d853163850dc47be
Harsh Dubey <harshdu001@gmail.com>
2026-02-09 00:49:16 +0530

    Fix case-sensitive imports for Vercel build

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
-import Header from './components/Header'
-import Body from './components/Body'
-import Footer from './components/Footer'
+import Header from "./Components/Header"
+import Body from './Components/Body'
+import Footer from './Components/Footer'
 import { Routes, Route, Outlet, Navigate} from "react-router-dom";
 import Error from './Pages/Error';
 import Contact from './Pages/Contact';
